342f059f90f06c1dfac4fbcadaa82a606e75fe18
Karthik-0304 <karthik.sivakumar004@gmail.com>
2025-12-09 20:13:30 +0530

    Add files via upload
    Java Assignment

---
+++ CheckNumberIsPositive.java
@@ -0,0 +1,12 @@
+package Assignment;
+
+public class CheckNumberIsPositive {
+	public static void main(String[] args) {
+		int a=10;
+		if (a>0)
+			System.out.println("Negative");
+		else
+			System.out.println("Positive");
+	}
+
+}
