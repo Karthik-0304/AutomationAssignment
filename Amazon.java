3a0d4716873481d8a9cfc4309d5d553c07769a1a
Karthik-0304 <karthik.sivakumar004@gmail.com>
2026-02-25 22:26:06 +0530

    Add files via upload

--- Amazon.java
+++ Amazon.java
@@ -1,52 +1,37 @@
 package sample;
 
+
 import java.time.Duration;
 import java.util.List;
 
-import org.openqa.selenium.*;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Amazon {
-
-    public static void main(String[] args) throws InterruptedException {
-
-        WebDriver driver = new FirefoxDriver();
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        driver.manage().window().maximize();
-        driver.get("https://www.amazon.in");
-        WebElement searchBox = wait.until(
-            ExpectedConditions.visibilityOfElementLocated(By.id("twotabsearchtextbox"))
-        );
-        searchBox.sendKeys("bag");
+	public static void main(String[] args) {
+		WebDriver driver=new ChromeDriver();
+		JavascriptExecutor jsExecutor=(JavascriptExecutor) driver;
+		
+		driver.get("https://www.amazon.in/");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.manage().window().maximize();
+		
+		driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']")).sendKeys("bag");
         driver.findElement(By.id("nav-search-submit-button")).click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(
-            By.cssSelector("div.s-search-results")
-        ));
-        int count = 0;
-        while (count < 5) {
-
-            List<WebElement> brands = driver.findElements(
-                By.xpath("//input[@type='checkbox' and contains(@name,'Brand')]")
-            );
-
-            if (brands.size() > count) {
-
-                WebElement brand = brands.get(count);
-                js.executeScript("arguments[0].click();", brand);
-                wait.until(ExpectedConditions.visibilityOfElementLocated(
-                    By.cssSelector("div.s-search-results")
-                ));
-
-                count++;
-            } else {
-                break;
-            }
-        }
-
-        System.out.println("First 5 brands selected successfully");
-        Thread.sleep(3000);
-        driver.quit();
-    }
+		
+		List<WebElement> brandsElements=driver.findElements(By.xpath("//span[text()='Brands']//following::span[@class='a-list-item']"));
+		
+		for (int i = 0; i < brandsElements.size(); i++) {
+			if(i==5) {
+				break;
+			}
+				WebElement brandElement=brandsElements.get(i);
+				if(!brandElement.isSelected()) {
+					jsExecutor.executeScript("arguments[0].click();", brandElement);
+			}
+		}
+	}
 }
